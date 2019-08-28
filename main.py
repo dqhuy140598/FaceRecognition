4c61f454aa0def82c35346dbbb41605add540547
Dao Quang Huy <daoquanghuy.uet.140598@gmail.com>
2019-08-28 17:16:16 +0700

    frist commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2
+
+class FaceDetector(object):
+    """docstring for FaceDetector."""
+
+    def __init__(self, arg):
+        super(FaceDetector, self).__init__()
+        self.arg = arg
