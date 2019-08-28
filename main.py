79d4d6d8fb8089c18ec07f6ba4193ef5eecca60d
Dao Quang Huy <daoquanghuy.uet.140598@gmail.com>
2019-08-28 17:19:45 +0700

    add gitignore

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import numpy as np
 import cv2
 
 class FaceDetector(object):
-    """docstring for FaceDetector."""
 
     def __init__(self, arg):
         super(FaceDetector, self).__init__()
         self.arg = arg
+
+    def run(self,name,number):
+        self.name = name
+        self.number = number
